d3f3061229ecaad8280177171eda79c8bb070414
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-05 15:51:08 -0400

    fix: add i18n types do app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,18 @@
 /// <reference types="unplugin-icons/types/svelte" />
 
+type Locales = import('$lib/i18n/i18n-types').Locales;
+type TranslationFunctions = import('$lib/i18n/i18n-types').TranslationFunctions;
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Error {}
-	// interface Locals {}
-	// interface PageData {}
-	// interface Platform {}
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Platform {}
+		// interface PageData {}
+		// interface Locals {}
+	}
 }
+
+export {};
