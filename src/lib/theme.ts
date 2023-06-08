846f2dddf030339e4bb6aeb1d29e67e7b87109d0
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-08 13:13:28 -0400

    feat/wip: themes

---
+++ src/lib/theme.ts
@@ -0,0 +1,37 @@
+export const themes = [
+	'light',
+	'dark',
+	'cupcake',
+	'bumblebee',
+	'emerald',
+	'corporate',
+	'synthwave',
+	'retro',
+	'cyberpunk',
+	'valentine',
+	'halloween',
+	'garden',
+	'forest',
+	'aqua',
+	'lofi',
+	'pastel',
+	'fantasy',
+	'wireframe',
+	'black',
+	'luxury',
+	'dracula',
+	'cmyk',
+	'autumn',
+	'business',
+	'acid',
+	'lemonade',
+	'night',
+	'coffee',
+	'winter',
+] as const;
+
+export type Theme = (typeof themes)[number];
+
+export function isTheme(theme: unknown): theme is Theme {
+	return typeof theme === 'string' && themes.includes(theme as Theme);
+}
