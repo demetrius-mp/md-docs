1280192c3e99cad95c521e70309ec0cada4bf0a8
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-15 22:09:25 -0400

    fix: use Container component

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { PUBLIC_APP_NAME } from '$env/static/public';
+	import Container from '$lib/components/Container.svelte';
 	import LL from '$lib/i18n/i18n-svelte';
 	import IconArrowRight from '~icons/mdi/ArrowRight';
 </script>
@@ -10,34 +11,32 @@
 	</title>
 </svelte:head>
 
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4 mb-4">
-	<div class="mx-auto max-w-6xl">
-		<a href="/app/docs" class="card bg-base-200 shadow-xl mb-6">
-			<div class="card-body p-5">
-				<div class="card-title flex justify-between">
-					{$LL.docs()}
-					<span class="self-start">
-						<IconArrowRight />
-					</span>
-				</div>
-				<p>
-					{$LL.manageDocs()}
-				</p>
+<Container>
+	<a href="/app/docs" class="card bg-base-200 shadow-xl mb-6">
+		<div class="card-body p-5">
+			<div class="card-title flex justify-between">
+				{$LL.docs()}
+				<span class="self-start">
+					<IconArrowRight />
+				</span>
 			</div>
-		</a>
+			<p>
+				{$LL.manageDocs()}
+			</p>
+		</div>
+	</a>
 
-		<a href="/app/settings" class="card bg-base-200 shadow-xl mb-6">
-			<div class="card-body p-5">
-				<div class="card-title flex justify-between">
-					{$LL.settings()}
-					<span class="self-start">
-						<IconArrowRight />
-					</span>
-				</div>
-				<p>
-					{$LL.manageSettings()}
-				</p>
+	<a href="/app/settings" class="card bg-base-200 shadow-xl mb-6">
+		<div class="card-body p-5">
+			<div class="card-title flex justify-between">
+				{$LL.settings()}
+				<span class="self-start">
+					<IconArrowRight />
+				</span>
 			</div>
-		</a>
-	</div>
-</div>
+			<p>
+				{$LL.manageSettings()}
+			</p>
+		</div>
+	</a>
+</Container>
